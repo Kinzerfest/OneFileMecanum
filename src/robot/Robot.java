9532de4ab0466e58c988cf0abdba189f37549ab7
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2018-09-09 09:23:57 -0500

    Added a speedmultiplier

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -5,54 +5,60 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.Servo;
+
 //import edu.wpi.first.wpilibj.SPI;
 //import com.kauailabs.navx.frc.AHRS;
 public class Robot extends TimedRobot {
-	//Variable declarations
-Spark leftFront, leftBack, rightFront, rightBack, climberMotor;
-Servo gearSlideMotor;
-Joystick primaryStick;
-MecanumDrive mecanumDrive;
-//AHRS navX;
+    //Variable declarations
+    Spark leftFront, leftBack, rightFront, rightBack, climberMotor;
+    Servo gearSlideMotor;
+    Joystick primaryStick;
+    MecanumDrive mecanumDrive;
+
+    //AHRS navX;
     public void robotInit() {
-    //Motors and Servos
-    leftFront = new Spark(2);
-    leftBack = new Spark(1);
-    rightFront = new Spark(0);
-    rightBack = new Spark(3);
-    climberMotor = new Spark(4);
-    gearSlideMotor = new Servo(5);
-	    
-    //navX = new AHRS(SPI.Port.kMXP); 
-    primaryStick = new Joystick(0);
-    //MecanumDrive 
-    mecanumDrive = new MecanumDrive(leftFront, leftBack, rightFront, rightBack);
-    mecanumDrive.setDeadband(0.25); //Sets deadzone for Mecanum Drive
+        //Motors and Servos
+        leftFront = new Spark(2);
+        leftBack = new Spark(1);
+        rightFront = new Spark(0);
+        rightBack = new Spark(3);
+        climberMotor = new Spark(4);
+        gearSlideMotor = new Servo(5);
+
+        //navX = new AHRS(SPI.Port.kMXP);
+        primaryStick = new Joystick(0);
+        //MecanumDrive
+        mecanumDrive = new MecanumDrive(leftFront, leftBack, rightFront, rightBack);
+        mecanumDrive.setDeadband(0.18); //Sets deadzone for Mecanum Drive
     }
+
     public void autonomousInit() {
     }
+
     public void autonomousPeriodic() {
     }
+
     public void teleopPeriodic() {
-    drive();
-    climber();
-    gearSlide();
-    //angleAdjustment();
+        drive();
+        climber();
+        gearSlide();
+        //angleAdjustment();
     }
+
     public void testPeriodic() {
     }
-    
+
     //--------------------------------------------Robot Methods------------------------------------------//
-    public void drive(){
-    	double turnThrottle = 0.0;
-	    //If the trigger is pressed, turn according to the joystick twist
-    	
-    	if(primaryStick.getRawButton(1)) {
-            turnThrottle = 0.6*primaryStick.getTwist();
-            } 
-    	else {
-    		turnThrottle = 0.0;
-    	}
+    public void drive() {
+        double turnThrottle = 0.0;
+        double speedMultiplier = (primaryStick.getZ() + 1) / 2;
+        //If the trigger is pressed, turn according to the joystick twist
+
+        if (primaryStick.getRawButton(1)) {
+            turnThrottle = 0.6 * primaryStick.getTwist();
+        } else {
+            turnThrottle = 0.0;
+        }
         /*
         if (primaryStick.getPOV() == -1) {
         if(primaryStick.getRawButton(1)) {
@@ -65,32 +71,33 @@ MecanumDrive mecanumDrive;
             turnThrottle = limit(angleDifference(navX.getAngle(), primaryStick.getPOV()) * 0.02, 0.4);
         }
         */
-        mecanumDrive.driveCartesian(primaryStick.getX(), -primaryStick.getY(), turnThrottle, 0);
+        mecanumDrive.driveCartesian(primaryStick.getX() * speedMultiplier, -primaryStick.getY() * speedMultiplier, turnThrottle, 0);
     }
-    
+
     public void gearSlide() {
-	    //Left buttons on the joystick
-	    //If both buttons are pressed, go to middle value.
-	    //If upper button is pressed, go to the upper position... ect....
-    if(primaryStick.getRawButton(5) && primaryStick.getRawButton(3)) {
-    	gearSlideMotor.set(0.48);
-    } else if(primaryStick.getRawButton(5)) {
-    	gearSlideMotor.set(0.73);
-    } else if(primaryStick.getRawButton(3)) {
-    	gearSlideMotor.set(0.35);
-    } 
+        //Left buttons on the joystick
+        //If both buttons are pressed, go to middle value.
+        //If upper button is pressed, go to the upper position... ect....
+        if (primaryStick.getRawButton(5) && primaryStick.getRawButton(3)) {
+            gearSlideMotor.set(0.48);
+        } else if (primaryStick.getRawButton(5)) {
+            gearSlideMotor.set(0.73);
+        } else if (primaryStick.getRawButton(3)) {
+            gearSlideMotor.set(0.35);
+        }
     }
-    
+
     public void climber() {
-	    //Right Buttons on the joystick
-    	if(primaryStick.getRawButton(6)) {
-    		climberMotor.set(1);
-    	} else if (primaryStick.getRawButton(4)) {
-    		climberMotor.set(-0.3);
-    	} else {
-    		climberMotor.set(0);
-    	}
+        //Right Buttons on the joystick
+        if (primaryStick.getRawButton(6)) {
+            climberMotor.set(1);
+        } else if (primaryStick.getRawButton(4)) {
+            climberMotor.set(-0.3);
+        } else {
+            climberMotor.set(0);
+        }
     }
+
     /*
     public void angleAdjustment() {
     	if(primaryStick.getRawButton(11)) {
@@ -103,20 +110,25 @@ MecanumDrive mecanumDrive;
     */
     //--------------------------------------------Utility Methods---------------------------------------//
     public static double limit(double value, double limit) {
-    	if (value > limit) {return limit;}
-    	else if (value < -limit) {return -limit;}
-    	else {return value;}
+        if (value > limit) {
+            return limit;
+        } else if (value < -limit) {
+            return -limit;
+        } else {
+            return value;
+        }
     }
-	public static double angleDifference(double startingAngle, double desiredAngle){
-		double difference;
-		difference = startingAngle - desiredAngle;
-		if (difference > -180 && difference <= 180)
-			return -difference;
-		else if (difference <= -180)
-			return -(difference + 360);
-		else if (difference > 180)
-			return -(difference - 360);
-		else 
-		return 0;
-	}
+
+    public static double angleDifference(double startingAngle, double desiredAngle) {
+        double difference;
+        difference = startingAngle - desiredAngle;
+        if (difference > -180 && difference <= 180)
+            return -difference;
+        else if (difference <= -180)
+            return -(difference + 360);
+        else if (difference > 180)
+            return -(difference - 360);
+        else
+            return 0;
+    }
 }
